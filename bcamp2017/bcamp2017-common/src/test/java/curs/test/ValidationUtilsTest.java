6849c304f2ea8e9e5fd5272a11b3b8ec1b6c2055
vmitache <v.mitache@gmail.com>
2017-04-02 18:28:21 +0300

    junit

---
+++ bcamp2017/bcamp2017-common/src/test/java/curs/test/ValidationUtilsTest.java
@@ -0,0 +1,35 @@
+package curs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import curs.exceptions.ValidationException;
+import curs.utils.ValidationUtils;
+
+public class ValidationUtilsTest {
+
+	@Test
+	public void testValidatePasswordValid() throws ValidationException {
+		ValidationUtils.validatePassword("01abcd02");
+	}
+	
+	@Test(expected=ValidationException.class)
+	public void testValidatePasswordInvalid() throws ValidationException {
+		ValidationUtils.validatePassword("xx");
+	}
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void testValidatePasswordInvalidSize() throws ValidationException {
+		exception.expect(ValidationException.class);
+		exception.expectMessage("minim 8 chars");
+		ValidationUtils.validatePassword("xx");
+	}
+
+	
+}
